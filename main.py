baf98df5b36c40cea19f7493272572f8e48953aa
Marlen <osmanov.m.i.20@gmail.com>
2025-11-23 06:44:43 +0300

    GET, POST, PATCH, DELETE requests with db connected

--- main.py
+++ main.py
@@ -1,11 +1,72 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Query
 from email_validator import validate_email, EmailNotValidError
+from sqlmodel import select
+
+from database import create_db_and_tables, Hero, SessionDep, HeroPublic, HeroCreate, HeroUpdate
 from schemas import Item, User, ModelName
 from validations import email_validation
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
 fake_items_db = [{"item_name": "Boo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.post("/heroes/", response_model=HeroPublic, tags=["heroes"])
+async def create_hero(hero: HeroCreate, session: SessionDep):
+    db_hero = Hero.model.validate(hero)
+    session.add(db_hero)
+    session.commit()
+    session.refresh(db_hero)
+    return db_hero
+
+
+@app.get("/heroes/", response_model=list[HeroPublic], tags=["heroes"])
+async def read_heroes(
+        session: SessionDep,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100,
+):
+    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+    return heroes
+
+
+@app.get("/heroes/{hero_id}", response_model=HeroPublic, tags=["heroes"])
+async def read_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
+
+
+@app.patch("/heroes/{hero_id}", response_model=HeroUpdate, tags=["heroes"])
+async def update_hero(hero_id: int, hero: HeroUpdate, session: SessionDep):
+    hero_db = session.get(Hero, hero_id)
+    if not hero_db:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    hero_data = hero.model_dump(exclude_unset=True)
+    hero_db.sqlmodel_update(hero_data)
+    session.add(hero_db)
+    session.commit()
+    session.refresh(hero_db)
+    return hero_db
+
+
+@app.delete("/heroes/{hero_id}", tags=["heroes"])
+async def delete_hero(hero_id: int, session: SessionDep):
+    hero = session.get(Hero, hero_id)
+    if not hero:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    session.delete(hero)
+    session.commit()
+    return {"message": "Hero deleted"}
 
 
 @app.get("/")
