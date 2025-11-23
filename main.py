151ffa684163e40feae89884f1cbce639d8733df
Marlen <osmanov.m.i.20@gmail.com>
2025-11-23 16:24:30 +0300

    main file update

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from typing import Annotated
 
-from fastapi import FastAPI, HTTPException, Query
-from email_validator import validate_email, EmailNotValidError
+from email_validator import EmailNotValidError, validate_email
+from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import select
 
-from database import create_db_and_tables, Hero, SessionDep, HeroPublic, HeroCreate, HeroUpdate
-from schemas import Item, User, ModelName
+from database import (
+    Hero,
+    HeroCreate,
+    HeroPublic,
+    HeroUpdate,
+    SessionDep,
+    create_db_and_tables,
+)
+from dependencies import get_query_token
+from internal import admin
+from routers import items, users
+from schemas import Item, ModelName, User
 from validations import email_validation
 
-app = FastAPI()
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_query_token)],
+    responses={404: {"description": "I'm a teapot"}},
+)
 
 
 @app.on_event("startup")
@@ -18,6 +39,9 @@ def on_startup():
 
 fake_items_db = [{"item_name": "Boo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
 
 @app.post("/heroes/", response_model=HeroPublic, tags=["heroes"])
 async def create_hero(hero: HeroCreate, session: SessionDep):
@@ -69,24 +93,25 @@ async def delete_hero(hero_id: int, session: SessionDep):
     return {"message": "Hero deleted"}
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, email: str, q: str | None = None):
-    try:
-        email = validate_email(email, check_deliverability=False)
-        email = email.normalized
-    except EmailNotValidError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    return {"item_id": item_id, "q": q, "email": email}
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_price": item.price, "item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, email: str, q: str | None = None):
+#     try:
+#         email = validate_email(email, check_deliverability=False)
+#         email = email.normalized
+#     except EmailNotValidError as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+#     return {"item_id": item_id, "q": q, "email": email}
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_price": item.price, "item_id": item_id}
+#
+#
+# @app.get("/items/")
+# async def read_items(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip: skip + limit]
 
 
 @app.get("/models/{model_name}")
@@ -100,12 +125,7 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
-@app.get("/items/")
-async def read_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
-
-@app.post("/user/")
-async def create_user(user: User):
-    email_validation(user.email)
-    return user
+# @app.post("/user/")
+# async def create_user(user: User):
+#     email_validation(user.email)
+#     return user
